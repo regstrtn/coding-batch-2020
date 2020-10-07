965da0d04a3dcc0106d73d6aed53c568be23a7e0
regstrtn <regstrtn@gmail.com>
2020-10-07 20:54:31 +0530

    Create helpermethods.h

---
+++ util/helpermethods.h
@@ -0,0 +1,17 @@
+#ifndef HELPER_METHODS_CLASS
+#define HELPER_METHODS_CLASS
+
+class TreeNode {
+public: 
+	int val; 
+	TreeNode * left; 
+	TreeNode * right; 
+	TreeNode(int val) {
+		this->val = val; 
+		this->left = NULL; 
+		this->right = NULL; 
+	}
+};
+
+#endif
+
