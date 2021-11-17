ccdc9ded79267f77ddec03f37b2a11cdd3944b42
regstrtn <regstrtn@gmail.com>
2021-11-17 23:31:15 +0530

    Update linkedlistdriver.cpp

--- drivercode/linkedlistdriver.cpp
+++ drivercode/linkedlistdriver.cpp
@@ -1,1 +1,48 @@
 // Linkedlist Driver Code 
+
+// Write include statements. 
+#include <iostream>
+#include <string> 
+#include <vector>
+
+use namespace std;
+
+// Linkedlist node
+class Node {
+  public: 
+  int val; 
+  Node * next; 
+}
+
+// Convert array to linkedlist
+Node * convertArrayToLinkedList(vector<int> & v) {
+  return new Node(); 
+}
+
+// Return an array of random integers of the given size. 
+vector<int> generateRandomArray(int size) {
+  return vector<int>(); 
+}
+
+// Insert into a linked list. 
+
+
+// Delete from a linked list. 
+
+
+// Compare linkedlists. 
+bool areEqual(Node *head1, Node * head2) {
+  return true; 
+}
+
+
+int main() {
+  // 1. Get a random array. 
+  // 2. Convert array to LinkedList List1.
+  // 3. Insert a number at position P into the list.
+  // 4. Delete this number from position P. 
+  // 5. Create another linkedlist (List2) from the same random array. 
+  // 6. Compare that List1 is the same as List2. 
+  
+  
+}
