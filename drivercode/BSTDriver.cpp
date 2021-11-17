671bf182cf11a4b847a1e831b410fb4def46b046
regstrtn <regstrtn@gmail.com>
2021-11-17 23:53:02 +0530

    Update BSTDriver.cpp

--- drivercode/BSTDriver.cpp
+++ drivercode/BSTDriver.cpp
@@ -4,3 +4,35 @@
 #include<vector>
 
 using namespace std; 
+
+class TreeNode {
+  public: 
+  int val; 
+  TreeNode * left; 
+  TreeNode * right; 
+}
+
+int main() {
+
+  // 1. Generate a random array. 
+  // 2. Create an empty tree and insert one value from the array at a time. 
+  // 3. Inorder traversal of this tree should be equal to the sorted value of random array.   
+  // 4. Find the height of the tree. 
+  
+  
+  // 5. Find the minimum value in the array. 
+  // 6. In the tree, find successor of this value. Print it. Then find successor
+  //    of the value just obtained. Print again and so on. 
+  // 7. This list should be equal to the sorted value of the array. 
+  
+  // 8. Find the maximum value in the array. 
+  // 9. In the tree, find predecessor of this value. Print it. Then find predecessor
+  //    of the value just obtained till no more predecessor exists. Print again and so on. 
+  // 10. This list should be equal to the sorted value of the array. 
+
+  // 11. Delete the last value of the array from the tree. 
+  // 12. Compare inorder traveral of this tree with random array. 
+  // 13. Insert the deleted value again. Compare the inorder traversal again. 
+  
+  
+}
